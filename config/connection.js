539652ad36dbe7595ef3e06cab3bcaf3437b145b
julhanlon <jhanlon289@gmail.com>
2020-06-24 20:13:30 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const password = require("../.env");
 
 
 let connection;
@@ -10,7 +11,7 @@ if(process.env.JAWSDB_URL){
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "Tlv12sf19!",
+        password: password,
         database: "todo_db",
 
 });
